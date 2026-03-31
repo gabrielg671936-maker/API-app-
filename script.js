75e869527e8573c5e77b6dc4fedd7770e67a6fd2
gabrielg671936-maker <gabrielg671936@gmail.com>
2026-03-31 09:40:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+fetch("https://tarotapi.dev/api/v1/cards/random?n=10")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (response) {
+    // handle ten random cards
+  })
+  .catch(function (error) {
+    // handle what went wrong
+  });
