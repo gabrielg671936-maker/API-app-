3af9dbe2b7c1e94c9b40d76130c261b3a0414711
gabrielg671936-maker <gabrielg671936@gmail.com>
2026-03-31 21:47:31 -0400

    Implement card drawing functionality on button click
    Added event listener to draw cards based on user input.

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
-fetch("https://tarotapi.dev/api/v1/cards/random?n=10")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    // handle ten random cards
-  })
-  .catch(function (error) {
-    // handle what went wrong
-  });
+const button = document.getElementById("drawButton");
+const input = document.getElementById("userQuestion");
+const container = document.querySelector(".cards");
+
+button.addEventListener("click", () => {
+  const question = input.value.trim();
+
+  if (!question) {
+    alert("Please enter a question for the cards!");
+    return;
+  }
 
 
-  //make variable called cards and do cards=response
+  // https://youtu.be/zUcc4vW-jsI needed a little help so i refereced to this video :)
+
+  fetch("https://tarotapi.dev/api/v1/cards/random?n=4")
+    .then(res => res.json())
+    .then(data => {
+      const cards = data.cards;
+      container.innerHTML = ""; 
+
+      cards.forEach(card => {
+        container.innerHTML += `
+          <div class="card">
+            <img src="${card.image}" alt="${card.name}" />
+            <h2>${card.name}</h2>
+            <p>${card.meaning_up}</p>
+          </div>
+        `;
+      });
+    })
+    .catch(err => {
+      container.innerHTML = "<p>Oops! Something went wrong.</p>";
+      console.error(err);
+    });
+});
+
+
