976ac9ffbe6ca1e3bcd08d1990057e46109b6710
gabrielg671936-maker <gabrielg671936@gmail.com>
2026-04-16 08:23:30 -0400

    Add new tarot card images to imageMap

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ const container = document.querySelector(".cards");
 const imageMap = {
   "The Fool": "images/the-fool.jpg",
   "The Magician": "images/the-magician.png",
-  "The Empress": "images/the-empress.png"
+  "The Empress": "images/the-empress.png",
+  "The Emperor": "images/the-emperor.png",
+  "The Hierophant": "images/the-hierophant.png",
+  "The Priestess": "images/the-high priestess.png",
+  "The Strength": "images/the-strength.png",
+  "The Hermit": "images/the-hermit.png",
+  "The Wheel of Fortune": "images/the-wheel.png",
 };
 
 button.addEventListener("click", () => {
